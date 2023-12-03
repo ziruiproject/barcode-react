b6dba40337142982048c163b76d08f2d17025422
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-12-03 19:49:31 +0700

    added store to firestore

--- src/Camera.jsx
+++ src/Camera.jsx
@@ -2,12 +2,35 @@ import { useState, useEffect } from "react";
 import { useZxing } from "react-zxing";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase";
+import { firestore } from "../firebase";
+import { collection, getDocs, query, where, orderBy, addDoc, serverTimestamp, onSnapshot } from 'firebase/firestore';
 
 export default function Camera() {
     const [result, setResult] = useState("");
     const { ref } = useZxing({
-        onDecodeResult(result) {
+        async onDecodeResult(result) {
             setResult(result.getText());
+            const user = auth.currentUser;
+
+            const userUid = user.uid;
+            const timestamp = serverTimestamp();
+
+            // Add the result to Firestore
+            const barcodeResultsCollection = collection(firestore, `history`);
+
+            // Create a new document with user UID, result, and timestamp
+            await addDoc(barcodeResultsCollection, {
+                userUid,
+                scanned: result.getText(),
+                timestamp,
+            })
+                .then(() => {
+                    console.log('Result added to Firestore successfully');
+                })
+                .catch((error) => {
+                    console.error('Error adding result to Firestore: ', error);
+                });
+
         },
     });
 
