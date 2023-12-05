46591a98a9b9fcbd2ec4e1cb9b90635324b0cbec
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-12-05 17:59:37 +0700

    made login UI

--- src/Login.jsx
+++ src/Login.jsx
@@ -34,15 +34,10 @@ export default function Login() {
             <h1 className="text-2xl font-bold">Selamat Datang!</h1>
             <form className="flex flex-col p-12 mt-12 bg-white rounded shadow-lg" action="">
                 <label className="text-xs font-semibold">Email</label>
-                <input placeholder='' className="focus:outline-none focus:ring-2 flex items-center w-64 h-12 px-4 mt-2 bg-gray-200 rounded" type="text" />
+                <input onChange={(e) => setEmail(e.target.value)} className="focus:outline-none focus:ring-2 flex items-center w-64 h-12 px-4 mt-2 bg-gray-200 rounded" type="text" />
                 <label className="mt-3 text-xs font-semibold">Password</label>
-                <input className="focus:outline-none focus:ring-2 flex items-center w-64 h-12 px-4 mt-2 bg-gray-200 rounded" type="password" />
-                <button onClick={handleRegis} className="hover:bg-blue-700 flex items-center justify-center w-64 h-12 px-6 mt-8 text-sm font-semibold text-blue-100 bg-blue-600 rounded">Masuk</button>
-                {/* <div className="flex justify-center mt-6 text-xs">
-                    <a className="hover:text-blue-500 text-blue-400" href="#">Lupa Password</a>
-                    <span className="mx-2 text-gray-300">/</span>
-                    <a className="hover:text-blue-500 text-blue-400" href="#">Daftar</a>
-                </div> */}
+                <input onChange={(e) => setPassword(e.target.value)} className="focus:outline-none focus:ring-2 flex items-center w-64 h-12 px-4 mt-2 bg-gray-200 rounded" type="password" />
+                <button onClick={handleRegis} type='button' className="hover:bg-blue-700 flex items-center justify-center w-64 h-12 px-6 mt-8 text-sm font-semibold text-blue-100 bg-blue-600 rounded">Masuk</button>
             </form>
         </div>
     );
