4b99830c1a7db87d9bd6a9249d58743b7b78ee0f
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-12-05 17:32:37 +0700

    made login UI

--- src/Login.jsx
+++ src/Login.jsx
@@ -30,14 +30,20 @@ export default function Login() {
     };
 
     return (
-        <div>
-            <h2>Login</h2>
-            <label>Email:</label>
-            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-            <label>Password:</label>
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            <button onClick={handleLogin}>Login</button>
-            <button onClick={handleRegis}>Regis</button>
+        <div class="flex flex-col items-center justify-center w-screen h-screen bg-gray-200 text-gray-700">
+            <h1 class="font-bold text-2xl">Selamat Datang! :)</h1>
+            <form class="flex flex-col bg-white rounded shadow-lg p-12 mt-12" action="">
+                <label class="font-semibold text-xs" for="usernameField">Email</label>
+                <input placeholder='' class="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2" type="text" />
+                <label class="font-semibold text-xs mt-3" for="passwordField">Password</label>
+                <input class="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2" type="password" />
+                <button class="flex items-center justify-center h-12 px-6 w-64 bg-blue-600 mt-8 rounded font-semibold text-sm text-blue-100 hover:bg-blue-700">Masuk</button>
+                {/* <div class="flex mt-6 justify-center text-xs">
+                    <a class="text-blue-400 hover:text-blue-500" href="#">Lupa Password</a>
+                    <span class="mx-2 text-gray-300">/</span>
+                    <a class="text-blue-400 hover:text-blue-500" href="#">Daftar</a>
+                </div> */}
+            </form>
         </div>
     );
 };
