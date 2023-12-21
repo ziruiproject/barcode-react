65cad47aa1bf6879ac5db27365d19ec2b8bfff70
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-12-21 21:13:24 +0700

    added report form

--- src/ScanHistory.jsx
+++ src/ScanHistory.jsx
@@ -56,7 +56,7 @@ export default function ScanHistory() {
 
     return (
         <div className="max-w-2xl mx-auto p-4">
-            <h1 className="text-3xl font-bold mb-4">Scan History</h1>
+            <h1 className="text-3xl font-bold mb-4">Riwayat Scan</h1>
             <div className="mb-4">
                 <label className="mr-2">Select Date:</label>
                 <DatePicker
