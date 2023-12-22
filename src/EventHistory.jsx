43141f873d209556641ea87ec38057b13130041d
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-12-22 19:00:34 +0700

    added loader

--- src/EventHistory.jsx
+++ src/EventHistory.jsx
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { collection, getDocs } from 'https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js';
 import { firestore } from './firebase';
+import { MutatingDots } from 'react-loader-spinner';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 export default function EventHistory() {
     const [reports, setReports] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [selectedDate, setSelectedDate] = useState(new Date());
 
     useEffect(() => {
         const fetchReports = async () => {
             try {
+                setLoading(true);
                 const reportsCollection = collection(firestore, 'reports');
                 const querySnapshot = await getDocs(reportsCollection);
                 const reportsData = querySnapshot.docs.map((doc) => doc.data());
@@ -25,22 +30,51 @@ export default function EventHistory() {
         fetchReports();
     }, []); // Empty dependency array ensures that the effect runs once when the component mounts
 
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
+    };
+
     if (loading) {
-        return <p>Loading...</p>;
+        return (
+            <div className="max-w-2xl mx-auto p-4 text-center">
+                <MutatingDots
+                    visible={true}
+                    height="100"
+                    width="100"
+                    color="#4fa94d"
+                    secondaryColor="#4fa94d"
+                    radius="12.5"
+                    ariaLabel="mutating-dots-loading"
+                    wrapperStyle={{}}
+                    wrapperClass=""
+                />
+            </div>
+        );
     }
 
     if (error) {
         return <p>Error: {error}</p>;
     }
 
+    const filteredReports = reports.filter((report) => {
+        const reportDate = new Date(report.date);
+        return (
+            reportDate.getFullYear() === selectedDate.getFullYear() &&
+            reportDate.getMonth() === selectedDate.getMonth() &&
+            reportDate.getDate() === selectedDate.getDate()
+        );
+    });
+
+
     return (
         <div className="max-w-2xl mx-auto p-4">
-            <h1 className="text-3xl font-bold mb-4">Laporan Kejadian</h1>
-            {reports.length === 0 ? (
-                <p className="text-gray-500">No reports available.</p>
+            <h1 className="text-3xl font-bold mb-4">Riwayat Kejadian</h1>
+            <DatePicker selected={selectedDate} onChange={handleDateChange} className="border p-2 mb-4" />
+            {filteredReports.length === 0 ? (
+                <p className="text-gray-500">Tidak ada kejadian pada tanggal ini.</p>
             ) : (
                 <ul>
-                    {reports.map((report, index) => (
+                    {filteredReports.map((report, index) => (
                         <li key={index} className="border p-4 mb-4">
                             <p className="font-bold mb-2">User: {report.userId}</p>
                             <p>Tanggal: {report.date}</p>
