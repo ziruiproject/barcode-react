95f827ce790a7d92d4ba6c998bf7a6260f91ac5c
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-12-22 17:43:42 +0700

    style event history page

--- src/EventHistory.jsx
+++ src/EventHistory.jsx
@@ -34,32 +34,34 @@ export default function EventHistory() {
     }
 
     return (
-        <div>
-            <h1>Event History</h1>
+        <div className="max-w-2xl mx-auto p-4">
+            <h1 className="text-3xl font-bold mb-4">Laporan Kejadian</h1>
             {reports.length === 0 ? (
-                <p>No reports available.</p>
+                <p className="text-gray-500">No reports available.</p>
             ) : (
                 <ul>
                     {reports.map((report, index) => (
-                        <li key={index}>
-                            <p>User ID: {report.userId}</p>
-                            <p>Date: {report.date}</p>
-                            <p>Title: {report.title}</p>
-                            <p>Location: {report.location}</p>
-                            <p>Description: {report.description}</p>
-                            <p>Timestamp: {report.timestamp.toDate().toLocaleString()}</p>
-                            <ul>
-                                {report.photos.map((photo, photoIndex) => (
-                                    <li key={photoIndex}>
-                                        <p>Photo ID: {photo.id}</p>
-                                        <img
-                                            src={photo.url}
-                                            alt={`Photo ${photoIndex}`}
-                                            style={{ maxWidth: '200px' }}
-                                        />
-                                    </li>
-                                ))}
-                            </ul>
+                        <li key={index} className="border p-4 mb-4">
+                            <p className="font-bold mb-2">User: {report.userId}</p>
+                            <p>Tanggal: {report.date}</p>
+                            <p>Judul: {report.title}</p>
+                            <p>Lokasi: {report.location}</p>
+                            <p>Deskripsi: {report.description}</p>
+                            <p>Waktu: {report.timestamp.toDate().toLocaleString()}</p>
+                            <div className="mt-4">
+                                <p className="font-bold mb-2">Gambar:</p>
+                                <ul className="grid grid-cols-2 gap-4">
+                                    {report.photos.map((photo, photoIndex) => (
+                                        <li key={photoIndex} className="border p-2">
+                                            <img
+                                                src={photo.url}
+                                                alt={`Photo ${photoIndex}`}
+                                                className="w-full h-auto"
+                                            />
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
                         </li>
                     ))}
                 </ul>
