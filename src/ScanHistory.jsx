e377248ab8288feded8ab076e2ea2c648b91aefd
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2024-01-31 11:36:28 +0700

    added superadmin selector

--- src/ScanHistory.jsx
+++ src/ScanHistory.jsx
@@ -8,10 +8,14 @@ import { Link } from 'react-router-dom';
 
 export default function ScanHistory() {
     const [selectedDate, setSelectedDate] = useState(new Date(new Date().setHours(0, 0, 0, 0)).getTime());
-    const [selectedOption, setSelectedOption] = useState("0");
+    const [selectedOption, setSelectedOption] = useState("self");
+    const [selectedRegu, setSelectedRegu] = useState("semua");
+    const [selectedArea, setSelectedArea] = useState("semua");
     const [userUid, setUserUid] = useState(null);
     const [historyData, setHistoryData] = useState([]);
     const [userData, setUserData] = useState(null)
+    const [reguOptions, setReguOptions] = useState([]);
+    const [areaOptions, setAreaOptions] = useState([]);
 
     const handleDateChange = (date) => {
         setSelectedDate(new Date(date.setHours(0, 0, 0, 0)));
@@ -19,6 +23,16 @@ export default function ScanHistory() {
 
     const handleSelectChange = (event) => {
         setSelectedOption(event.target.value);
+    };
+
+    const handleReguChange = (event) => {
+        console.log(event.target.value)
+        setSelectedRegu(event.target.value);
+    };
+
+    const handleAreaChange = (event) => {
+        console.log(event.target.value)
+        setSelectedArea(event.target.value);
     };
 
     const fetchData = async () => {
@@ -38,7 +52,7 @@ export default function ScanHistory() {
             const ref = collection(firestore, 'histories');
             let historyQuery;
 
-            if (selectedOption === "0") {
+            if (selectedOption === "self") {
                 // Option 0: Fetch all histories for the user on the selected date
                 historyQuery = query(
                     ref,
@@ -46,7 +60,7 @@ export default function ScanHistory() {
                     where('timestamp', '>=', startOfDayTimestamp),
                     where('timestamp', '<=', endOfDayTimestamp)
                 );
-            } else if (selectedOption === "1") {
+            } else if (selectedOption === "regu") {
                 // Option 1: Fetch histories for the user's reguId on the selected date
                 const userReguId = userData.reguId;
                 historyQuery = query(
@@ -55,7 +69,7 @@ export default function ScanHistory() {
                     where('timestamp', '>=', startOfDayTimestamp),
                     where('timestamp', '<=', endOfDayTimestamp)
                 );
-            } else if (selectedOption === "2") {
+            } else if (selectedOption === "area") {
                 // Option 1: Fetch histories for the user's reguId on the selected date
                 const userAreaId = userData.areaId;
                 historyQuery = query(
@@ -64,6 +78,37 @@ export default function ScanHistory() {
                     where('timestamp', '>=', startOfDayTimestamp),
                     where('timestamp', '<=', endOfDayTimestamp)
                 );
+            } else if (selectedOption === "admin") {
+                // Option 1: Fetch histories for the user's reguId on the selected date
+                if (selectedRegu === "semua" && selectedArea === "semua") {
+                    historyQuery = query(
+                        ref,
+                        where('timestamp', '>=', startOfDayTimestamp),
+                        where('timestamp', '<=', endOfDayTimestamp)
+                    );
+                } else if (selectedRegu === "semua") {
+                    historyQuery = query(
+                        ref,
+                        where('userData.areaId', '==', selectedArea),
+                        where('timestamp', '>=', startOfDayTimestamp),
+                        where('timestamp', '<=', endOfDayTimestamp)
+                    );
+                } else if (selectedArea === "semua") {
+                    historyQuery = query(
+                        ref,
+                        where('userData.reguId', '==', selectedRegu),
+                        where('timestamp', '>=', startOfDayTimestamp),
+                        where('timestamp', '<=', endOfDayTimestamp)
+                    );
+                } else {
+                    historyQuery = query(
+                        ref,
+                        where('userData.reguId', '==', selectedRegu),
+                        where('userData.areaId', '==', selectedArea),
+                        where('timestamp', '>=', startOfDayTimestamp),
+                        where('timestamp', '<=', endOfDayTimestamp)
+                    );
+                }
             }
 
             const historyDocs = await getDocs(historyQuery);
@@ -73,6 +118,29 @@ export default function ScanHistory() {
             console.error('Error fetching history data:', error);
         }
     };
+
+    useEffect(() => {
+        const fetchReguOptions = async () => {
+            const reguQuery = query(collection(firestore, 'regu'));
+            const reguDocs = await getDocs(reguQuery);
+
+            const reguData = reguDocs.docs.map(docSnapshot => docSnapshot.data());
+
+            setReguOptions(reguData);
+        };
+
+        const fetchAreaOptions = async () => {
+            const areaQuery = query(collection(firestore, 'area'));
+            const areaDocs = await getDocs(areaQuery);
+
+            const areaData = areaDocs.docs.map(docSnapshot => docSnapshot.data());
+
+            setAreaOptions(areaData);
+        };
+
+        fetchReguOptions();
+        fetchAreaOptions();
+    }, []);
 
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(async (user) => {
@@ -107,7 +175,7 @@ export default function ScanHistory() {
 
     useEffect(() => {
         fetchData();
-    }, [userUid, selectedDate, selectedOption]);
+    }, [userUid, selectedDate, selectedOption, selectedArea, selectedRegu]);
 
     return (
         <div className="gap-y-5 grid max-w-2xl p-4 mx-auto">
@@ -136,32 +204,71 @@ export default function ScanHistory() {
                             className="block w-full p-2 bg-white border rounded-md"
                             value={selectedOption}
                             onChange={handleSelectChange}>
-                            <option value="0">Diri Sendiri</option>
-                            <option value="1">Satu Regu</option>
-                            {userData && userData.roleId > 2 && (
-                                <option value="2">Satu Area</option>
+                            <option value="self">Diri Sendiri</option>
+                            <option value="regu">Satu Regu</option>
+                            {userData.roleId > 2 && (
+                                <option value="area">Satu Area</option>
+                            )}
+                            {userData.roleId == 99 && (
+                                <option value="admin">Superadmin</option>
                             )}
                         </select>
                     </div>
+                )}
+                {userData && userData.roleId == 99 && selectedOption === "admin" && (
+                    <>
+                        <div className='gap-y-1 grid'>
+                            <label>Cari Berdasarkan Regu</label>
+                            <select
+                                className="block w-full p-2 bg-white border rounded-md"
+                                value={selectedRegu}
+                                onChange={handleReguChange}>
+                                <option key="semua" value="semua">
+                                    Semua
+                                </option>
+                                {reguOptions.map(regu => (
+                                    <option key={regu.id} value={regu.id}>
+                                        {regu.id}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                        <div className='gap-y-1 grid'>
+                            <label>Cari Berdasarkan Area</label>
+                            <select
+                                className="block w-full p-2 bg-white border rounded-md"
+                                value={selectedArea}
+                                onChange={handleAreaChange}>
+                                <option key="semua" value="semua">
+                                    Semua
+                                </option>
+                                {areaOptions.map(area => (
+                                    <option key={area.id} value={area.id}>
+                                        {area.id}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                    </>
                 )}
             </div>
             <div className='gap-y-5 grid'>
                 {historyData.map((historyItem, index) => (
                     <div key={index} className=" rounded-xl gap-y-5 grid p-4 shadow-md">
-                        <div className='gap-x-2 flex'>
-                            <span className='mr-auto text-xl font-medium'>{historyItem.userData.displayName}</span>
+                        <div className=' gap-x-2 flex'>
                             <span className='w-fit px-3 py-1 text-sm text-blue-500 bg-blue-100 rounded-full'>Regu {historyItem.userData.reguId}</span>
                             <span className='w-fit px-3 py-1 text-sm text-blue-500 bg-blue-100 rounded-full'>Area {historyItem.userData.areaId}</span>
                         </div>
+                        <span className='mr-auto text-xl font-medium'>{historyItem.userData.displayName}</span>
                         <span className='text-xl'>{historyItem.scanned}</span>
                         <div className='flex justify-between'>
                             <span className='w-fit px-3 py-1 text-sm text-blue-500 bg-blue-100 rounded-full'>{new Date(historyItem.timestamp * 1000).toLocaleTimeString('en-US', { hour12: false })} WIB</span>
                             <span className='w-fit gap-x-1 flex px-3 py-1 text-sm text-blue-500 bg-blue-100 rounded-full'>
                                 <span className=''>
-                                    {historyItem.coordinates.latitude}
+                                    {historyItem.coordinates?.latitude ? historyItem.coordinates?.latitude : "NaN"}
                                 </span>,
                                 <span className=''>
-                                    {historyItem.coordinates.longitude}
+                                    {historyItem.coordinates?.longitude ? historyItem.coordinates?.longitude : "NaN"}
                                 </span>
                             </span>
                         </div>
